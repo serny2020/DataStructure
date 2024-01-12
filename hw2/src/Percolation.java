d750428e3e2c31150dbf4fbb4fa88a1d12c4786d
xiaocheng sun <xiaocheng.sun@tufts.edu>
2024-01-11 22:15:16 -0500

    fixed backwash and add some tests

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -7,36 +7,65 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
  * Percolation simulation of Monte Carlo Model
  */
 public class Percolation {
-    private boolean[] board;
-    private int boardSide;
+    private final boolean[] board;
+    private final int boardSide;
     private final int VIRTUAL_TOP;
-    private final int VIRTUAL_BUTTOM;
-    private WeightedQuickUnionUF connectionBoard;
+    private final int VIRTUAL_BOTTOM;
+    private final WeightedQuickUnionUF connectionBoard; //used to check if water goes through
+    private final WeightedQuickUnionUF antiBackWash; //used to avoid water going backward
+    private int openSites;
 
     public Percolation(int N) {
         boardSide = N;
         VIRTUAL_TOP = N * N;
-        VIRTUAL_BUTTOM = N * N + 1;
-        //initialize an array of board to keep track if any cell is open
-        board = new boolean[N * N + 1];
-        //initialize a connectionBoard with virtual top and button.
+        VIRTUAL_BOTTOM = N * N + 1;
+        openSites = 0;
+        //initialize an array of board to keep track if any tile is open
+        board = new boolean[N * N];
+        //connectionBoard connect both virtual top and button.
         connectionBoard = new WeightedQuickUnionUF(N * N + 2);
-        //connect all the top squares with the virtual top
-        for (int i = 0; i < boardSide; i++) {
-            connectionBoard.union(i, VIRTUAL_TOP);
+        //antiBackWash only connect to the virtual top
+        antiBackWash = new WeightedQuickUnionUF(N * N + 1);
+        //connect all the top tiles with the virtual top
+        for (int col = 0; col < boardSide; col++) {
+            connectionBoard.union(col, VIRTUAL_TOP);
+            antiBackWash.union(col, VIRTUAL_TOP);
+        }
+        //connect all the bottom with the virtual bottom
+        for (int col = xyTo1D(boardSide - 1, 0); col < xyTo1D(boardSide - 1, boardSide - 1); col++) {
+            connectionBoard.union(col, VIRTUAL_BOTTOM);
         }
 
     }
 
+    /**
+     * convert 2D index to 1D representation
+     *
+     * @param x of row index
+     * @param y of column index
+     * @return 1D representation in the 2D board
+     */
     private int xyTo1D(int x, int y) {
         return boardSide * x + y;
     }
 
     /**
+     * Check if row or column is within the bound
+     *
+     * @param row index
+     * @param col index
+     */
+    private void isValidIndex(int row, int col) {
+        if (row < 0 || row > boardSide || col < 0 || col > boardSide) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+    }
+
+    /**
      * Get the 1D representation of all the neighbors, -1 if there is no such neighbor.
      *
-     * @param row index of the current cell
-     * @param col index of the current cell
+     * @param row index of the current tile
+     * @param col index of the current tile
      * @return an int array of 1D representation of all the neighbors, -1 if no such neighbor exists.
      */
     private int[] getNeighbors(int row, int col) {
@@ -63,54 +92,78 @@ public class Percolation {
     /**
      * Check if any neighbors is open, and connect them if there is any.
      *
-     * @param row index of the current cell
-     * @param col index of the current cell
+     * @param row index of the current tile
+     * @param col index of the current tile
      */
     private void connectOpenNeighbors(int row, int col) {
-        for (int cell : getNeighbors(row, col)) {
-            //if any valid neighbor is open, connect this cell with neighbors
-            if (cell != -1 && board[cell]) {
-                connectionBoard.union(xyTo1D(row, col), cell);
+        for (int tile : getNeighbors(row, col)) {
+            //if any valid neighbor is open, connect this tile with neighbors
+            if (tile != -1 && board[tile]) {
+                connectionBoard.union(xyTo1D(row, col), tile);
+                antiBackWash.union(xyTo1D(row, col), tile);
             }
         }
     }
 
+    /**
+     * Open the current tile and connect with its open neighbors if there is any exists
+     *
+     * @param row index of current tile
+     * @param col index of current tile
+     */
     public void open(int row, int col) {
+        isValidIndex(row, col);
         if (!board[xyTo1D(row, col)]) {
             board[xyTo1D(row, col)] = true;
+            openSites += 1;
             connectOpenNeighbors(row, col);
         }
     }
 
+    /**
+     * Check if a given tile is open.
+     *
+     * @param row index of current tile
+     * @param col index of current tile
+     * @return true if the tile is open; false otherwise
+     */
     public boolean isOpen(int row, int col) {
+        isValidIndex(row, col);
         return board[xyTo1D(row, col)];
     }
 
     /**
-     * @param row
-     * @param col
-     * @return true if the cell is full; false otherwise
+     * check if the given tile is filled with water
+     *
+     * @param row index of current tile
+     * @param col index of current tile
+     * @return true if the tile is full; false otherwise
      */
     public boolean isFull(int row, int col) {
-        //return true if current cell is open and connected to the top
-        if (isOpen(row, col) && connectionBoard.connected(xyTo1D(row, col), VIRTUAL_TOP)) {
-            return true;
-        } else {
+        isValidIndex(row, col);
+        //return true if current tile is open and connected to the top
+        return (isOpen(row, col) && antiBackWash.connected(xyTo1D(row, col), VIRTUAL_TOP));
+    }
+
+    /**
+     * Accessing the number of open space.
+     * This method takes constant time since it simply returns the value of the global variable.
+     *
+     * @return the number of open tiles
+     */
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+    /**
+     * Check if the system percolated
+     *
+     * @return true if the virtual top is connected with the virtual bottom
+     */
+    public boolean percolates() {
+        if (numberOfOpenSites() < boardSide) {
             return false;
         }
+        return (connectionBoard.connected(VIRTUAL_TOP, VIRTUAL_BOTTOM));
     }
-
-    public int numberOfOpenSites() {
-        // TODO: Fill in this method.
-        return 0;
-    }
-
-    public boolean percolates() {
-        // TODO: Fill in this method.
-        return false;
-    }
-
-    // TODO: Add any useful helper methods (we highly recommend this!).
-    // TODO: Remove all TODO comments before submitting.
-
 }
--- hw2/tests/PercolationTest.java
+++ hw2/tests/PercolationTest.java
@@ -3,6 +3,18 @@ import org.junit.jupiter.api.Test;
 import static com.google.common.truth.Truth.assertThat;
 
 public class PercolationTest {
+    private int[][] getSmallState(int N, Percolation p) {
+        int[][] state = new int[3][3];
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                int open = p.isOpen(r, c) ? 1 : 0;
+                int full = p.isFull(r, c) ? 2 : 0;
+                state[r][c] = open + full;
+            }
+        }
+        return state;
+    }
+
     private int[][] getState(int N, Percolation p) {
         int[][] state = new int[5][5];
         for (int r = 0; r < N; r++) {
@@ -14,6 +26,7 @@ public class PercolationTest {
         }
         return state;
     }
+
     @Test
     public void basicTest() {
         int N = 5;
@@ -41,8 +54,88 @@ public class PercolationTest {
     }
 
     @Test
-    public void yourTestHere() {
-        // TODO: write some more tests
+    public void fullTileTest() {
+        int N = 3;
+        Percolation p = new Percolation(3);
+        int[][] openSites = {
+                {0, 0},
+                {0, 1},
+                {0, 2},
+                {1, 0},
+                {1, 1},
+                {1, 2},
+                {2, 0},
+                {2, 1},
+                {2, 2}
+        };
+        int[][] expectedState = {
+                {3, 3, 3},
+                {3, 3, 3},
+                {3, 3, 3},
+        };
+        for (int[] site : openSites) {
+            p.open(site[0], site[1]);
+        }
+        assertThat(getSmallState(N, p)).isEqualTo(expectedState);
+        assertThat(p.percolates()).isTrue();
     }
+
+
+    @Test
+    public void percolatesTest() {
+        int N = 5;
+        Percolation p = new Percolation(5);
+        int[][] openSites = {
+                {0, 1},
+                {2, 0},
+                {3, 1},
+                {4, 1},
+                {1, 0},
+                {1, 1},
+                {2, 1}
+        };
+        int[][] expectedState = {
+                {0, 3, 0, 0, 0},
+                {3, 3, 0, 0, 0},
+                {3, 3, 0, 0, 0},
+                {0, 3, 0, 0, 0},
+                {0, 3, 0, 0, 0}
+        };
+        for (int[] site : openSites) {
+            p.open(site[0], site[1]);
+        }
+        assertThat(getState(N, p)).isEqualTo(expectedState);
+        assertThat(p.percolates()).isTrue();
+    }
+
+    @Test
+    public void backwashTest() {
+        int N = 5;
+        Percolation p = new Percolation(5);
+        int[][] openSites = {
+                {0, 1},
+                {2, 0},
+                {3, 1},
+                {4, 1},
+                {1, 0},
+                {1, 1},
+                {2, 1},
+                {4, 3},
+                {3, 3},
+                {2, 3}
+        };
+        int[][] expectedState = {
+                {0, 3, 0, 0, 0},
+                {3, 3, 0, 0, 0},
+                {3, 3, 0, 1, 0},
+                {0, 3, 0, 1, 0},
+                {0, 3, 0, 1, 0}
+        };
+        for (int[] site : openSites) {
+            p.open(site[0], site[1]);
+        }
+        assertThat(getState(N, p)).isEqualTo(expectedState);
+    }
+
 
 }
